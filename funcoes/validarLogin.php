a9c69791a958dddb7a88071fd64b2c31153d5b67
Francisco Duarte <57569921+Wyzo@users.noreply.github.com>
2022-01-21 16:20:51 +0000

    Changed login system

--- funcoes/validarLogin.php
+++ funcoes/validarLogin.php
@@ -2,27 +2,55 @@
 
 session_start();
 
+$utilizadores = [
+    "admin@gmail.com" => "admin123",
+    "utilizador@gmail.com" => "utilizador123"
+];
+
+
+if(isset($_POST["email"]) && !isset($_SESSION["email"])){
+    if($utilizadores[$_POST["email"]] == $_POST["password"]){
+
+        $_SESSION["email"] = $_POST["email"];
+    }
+
+    if(!isset($_SESSION["email"]))
+    {
+        $erro = true;
+    }
+}
+
+if(isset($_SESSION["email"])){
+    if($_SESSION["email"] == "admin@gmail.com"){
+        header('location:../dashboard.php');
+        $_SESSION["TIPO_CONTA"] = "admin";
+        $_SESSION["LOGADO"] = "true";
+    }
+    else if($_SESSION["email"] == "utilizador@gmail.com"){
+        header('location:../index.php');
+        $_SESSION["TIPO_CONTA"] = "utilizador";
+        $_SESSION["LOGADO"] = "true";
+    }
+}
+
+if($erro == true){header("Location: ../login.php?err=true");}
+
 /*
-ADMIN
-
-email: admin@gmail.com
-pass:  admin123
-
-
-UTILIZADOR
-
-email: utilizador@gmail.com
-pass: utilizador123
-
-*/
-
-$email = $_POST["email"];
-$pass = $_POST["password"];
-
-$_SESSION["TIPO_CONTA"] = "visitante";
-$_SESSION["LOGADO"] = "false";
-$_SESSION["email"] = $email;
-
+if($existeUtilizador)
+{
+    if($email == "admin@gmail.com" and $pass == "admin123")
+    {
+        header('location:../dashboard.php');
+        $_SESSION["TIPO_CONTA"] = "admin";
+        $_SESSION["LOGADO"] = "true";
+    }
+    else if($email == "utilizador@gmail.com" and $pass == "utilizador123")
+    {
+        header('location:../index.php');
+        $_SESSION["TIPO_CONTA"] = "utilizador";
+        $_SESSION["LOGADO"] = "true";
+    }
+}
 
 if($email == "admin@gmail.com" and $pass == "admin123")
 {
@@ -40,5 +68,5 @@ else
 {
     header("Location: ../login.php?err=true");
 }
-
+*/
 ?>
